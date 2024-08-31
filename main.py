22e629dca05d6e84425ae8f9777cdeb179dbb708
Luis Millones <luismillonesutc9090@gmail.com>
2024-08-31 16:56:19 -0500

    function suma

---
+++ main.py
@@ -0,0 +1,9 @@
+def suma(a, b):
+    """
+    Esta función recibe dos números y retorna su suma.
+
+    :param a: El primer número a sumar.
+    :param b: El segundo número a sumar.
+    :return: La suma de a y b.
+    """
+    return a + b
